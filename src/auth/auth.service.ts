e61e94826f75ee24de2cf8353e4bc507a83adb16
Manjula Dube <dube.manjula668@gmail.com>
2018-09-23 13:20:42 +0200

    add user service in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+     private readonly usersService: UsersService,
+     private readonly jwtService: JwtService,
+  ) {}
 
   async createToken(id: string) {
     const user: JwtPayload = { email: 'test@email.com' };
