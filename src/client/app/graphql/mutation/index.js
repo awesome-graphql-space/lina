5e84d50709f3a813bb3d767b2fcddbd0a0efee12
Manjula Dube <dube.manjula668@gmail.com>
2018-11-20 22:57:57 +0100

    add mutattion

---
+++ src/client/app/graphql/mutation/index.js
@@ -0,0 +1,42 @@
+import gql from 'graphql-tag';
+
+export const LOGIN = gql`
+    mutation login($username: String!, $password: String!){
+        login(username: $username, password: $password){
+            token
+            user {
+                id
+                username
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation loginUser($isLoggedIn: Boolean!, $token: String!){
+        loginUser(isLoggedIn: $isLoggedIn, token: $token) @client{
+            id
+            loggedIn
+            token
+        }
+    }
+`;
+
+export const STORE_TOKEN = gql`
+    mutation storeToken($loggedIn: Boolean!){
+        storeToken(loggedIn: $loggedIn) @client
+    }
+`;
+
+export const SIGNUP = gql`
+    mutation signup($username: String!, $password: String!, $displayName: String!){
+        signup(username: $username, password: $password, displayName: $displayName ){
+            token
+            user {
+                id
+                username
+                displayName
+            }
+        }
+    }
+`;
